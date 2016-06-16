66a456fb2dbba036dfe8f0b5d687371389bb0a39
Gregory R. Lee <gregory.lee@cchmc.org>
2016-06-16 10:52:21 -0400

    fix example

--- examples/example.py
+++ examples/example.py
@@ -1,18 +1,25 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-import skimage.data
-from skimage import img_as_float
-from skimage.transform import resize
+try:
+    import skimage.data
+    from skimage import img_as_float
+    from skimage.transform import resize
+except ImportError:
+    raise ImportError("This example requires scikit-image")
 
 from FFST import (scalesShearsAndSpectra,
                   inverseShearletTransformSpect,
                   shearletTransformSpect)
+from FFST._fft import ifftnc  # centered nD inverse FFT
 
 
-def ifft2(x):
-    """ centered inverse FFT """
-    return np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(Psi[..., idx])))
+def add_cbar(im, ax):
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="10%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+
 
 X = img_as_float(skimage.data.camera())
 X = resize(X, (256, 256))
@@ -39,7 +46,7 @@ axes[1, 0].imshow(Psi_shifted, interpolation='nearest', cmap=plt.cm.gray)
 axes[1, 0].set_axis_off()
 axes[1, 0].set_title('shearlet in Fourier domain')
 
-axes[1, 1].imshow(np.abs(ifft2(Psi_shifted)),
+axes[1, 1].imshow(np.abs(ifftnc(Psi_shifted)),
                   interpolation='nearest', cmap=plt.cm.gray)
 axes[1, 1].set_axis_off()
 axes[1, 1].set_title('shearlet in time domain')
@@ -59,40 +66,43 @@ plt.colorbar()
 plt.title('Transform Exactness')
 
 
-# compute shearlet transform
-ST, Psi = shearletTransformSpect(X, realCoefficients=False)
-idx = 13
-fig, axes = plt.subplots(3, 2)
+if False:
+    # complex shearlet example
 
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-def add_cbar(im, ax):
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="10%", pad=0.05)
-    plt.colorbar(im, cax=cax)
+    # compute shearlet transform
+    ST, Psi = shearletTransformSpect(X, realCoefficients=False)
+    idx = 13
+    fig, axes = plt.subplots(3, 2)
 
-cmap = plt.cm.jet
-im = axes[0, 0].imshow(X, interpolation='nearest', cmap=cmap)
-add_cbar(im0, axes[0, 0])
-axes[0, 0].set_axis_off()
-axes[0, 0].set_title('original image')
-axes[0, 1].set_axis_off()
+    cmap = plt.cm.jet
+    im = axes[0, 0].imshow(X, interpolation='nearest', cmap=cmap)
+    add_cbar(im, axes[0, 0])
+    axes[0, 0].set_axis_off()
+    axes[0, 0].set_title('original image')
+    axes[0, 1].set_axis_off()
 
-im = axes[1, 0].imshow(ST[..., idx].real, interpolation='nearest', cmap=cmap)
-add_cbar(im, axes[1, 0])
-axes[1, 0].set_axis_off()
-axes[1, 0].set_title('shearlet coefficients (real part)')
+    im = axes[1, 0].imshow(ST[..., idx].real, interpolation='nearest',
+                           cmap=cmap)
+    add_cbar(im, axes[1, 0])
+    axes[1, 0].set_axis_off()
+    axes[1, 0].set_title('shearlet coefficients (real part)')
 
-im = axes[1, 1].imshow(ST[..., idx].imag, interpolation='nearest', cmap=cmap)
-add_cbar(im, axes[1, 1])
-axes[1, 1].set_axis_off()
-axes[1, 1].set_title('shearlet coefficients (imaginary part)')
+    im = axes[1, 1].imshow(ST[..., idx].imag, interpolation='nearest',
+                           cmap=cmap)
+    add_cbar(im, axes[1, 1])
+    axes[1, 1].set_axis_off()
+    axes[1, 1].set_title('shearlet coefficients (imaginary part)')
 
-im = axes[2, 0].imshow(np.abs(ST[..., idx]), interpolation='nearest', cmap=cmap)
-add_cbar(im, axes[2, 0])
-axes[2, 0].set_axis_off()
-axes[2, 0].set_title('shearlet coefficients (absolute value)')
+    im = axes[2, 0].imshow(np.abs(ST[..., idx]), interpolation='nearest',
+                           cmap=cmap)
+    add_cbar(im, axes[2, 0])
+    axes[2, 0].set_axis_off()
+    axes[2, 0].set_title('shearlet coefficients (absolute value)')
 
-im = axes[2, 1].imshow(np.angle(ST[..., idx]), interpolation='nearest', cmap=cmap)
-add_cbar(im, axes[2, 1])
-axes[2, 1].set_axis_off()
-axes[2, 1].set_title('shearlet coefficients (phase)')
+    im = axes[2, 1].imshow(np.angle(ST[..., idx]), interpolation='nearest',
+                           cmap=cmap)
+    add_cbar(im, axes[2, 1])
+    axes[2, 1].set_axis_off()
+    axes[2, 1].set_title('shearlet coefficients (phase)')
+
+plt.show()
