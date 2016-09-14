725c0469790265d2d0d761c4d4056f7f8c6e8bb4
Gregory R. Lee <grlee77@gmail.com>
2016-09-14 01:11:57 -0400

    BLD: use setuptools in setup.py

--- setup.py
+++ setup.py
@@ -1,45 +1,17 @@
 #!/usr/bin/env python
-'''
-Installation script for FFST
+from setuptools import setup, find_packages
 
-Note:
-To make a source distribution:
-python setup.py sdist
-
-To make an RPM distribution:
-python setup.py bdist_rpm
-
-To Install:
-python setup.py install --prefix=/usr/local
-
-See also:
-python setup.py bdist --help-formats
-'''
-
-import os
-from numpy.distutils.core import setup
-from numpy.distutils.misc_util import Configuration
 import versioneer
 
-
-def configuration(parent_package='', top_path=None):
-    if os.path.exists('MANIFEST'):
-        os.remove('MANIFEST')
-
-    config = Configuration(None, parent_package, top_path)
-
-    # main modules
-    config.add_subpackage('FFST')
-    config.add_subpackage('FFST.tests')
-
-    return config
-
-
 setup(name='PyShearlets',
+      packages=find_packages(),
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description='2D fast finite shearlet transforms.',
       author='Gregory R. Lee',
       author_email='grlee77@gmail.com',
       url='https://bitbucket.org/grlee77/FFST',
-      configuration=configuration)
+      license='BSD 3-clause',
+      zip_safe=False,
+      # ackage_data={},
+      )
